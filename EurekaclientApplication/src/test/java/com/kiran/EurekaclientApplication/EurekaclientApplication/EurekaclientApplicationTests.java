54a3660e9d241dc2dab383ec168e9a16acd38d64
Kiran-Maharjan <kiranmaharjan8888@gmail.com>
2022-10-11 00:03:19 +0545

    Client are listed in server of Eureka

---
+++ EurekaclientApplication/src/test/java/com/kiran/EurekaclientApplication/EurekaclientApplication/EurekaclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiran.EurekaclientApplication.EurekaclientApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
